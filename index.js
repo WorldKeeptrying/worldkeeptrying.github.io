939cf4ff0022a783607b1a7bfbd68643864b72bf
WorldKeeptrying <72200962+WorldKeeptrying@users.noreply.github.com>
2021-07-12 13:08:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+﻿let deferredPrompt;
+// 默認不展示按鈕，僅支持 「Add to Home Screen」 功能才展現
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+// 規定必須註冊 serviceWorker 才能使用 Add to Home Screen，
+// 且需要監聽 install 和 fetch 事件，可以不處理
+if('serviceWorker' in navigator) {
+navigator.serviceWorker
+.register('./sw2.js')
+.then(function() { console.log('Service Worker Registered'); });
+}
+// 僅瀏覽器支持且未安裝該應用，以下事件才會觸發
+window.addEventListener('beforeinstallprompt', (e) => {
+// Chrome 67 及之前版本，會自動展現安裝的 prompt
+// 為了版本統一及用戶體驗，我們禁止自動展現 prompt
+e.preventDefault();
+// 存放事件用於後續觸發
+deferredPrompt = e;
+// 展現按鈕
+addBtn.style.display = 'block';
+addBtn.addEventListener('click', (e) => {
+// hide our user interface that shows our A2HS button
+addBtn.style.display = 'none';
+// 展現安裝的 prompt
+deferredPrompt.prompt();
+// 等待用戶對 prompt 進行操作
+// 如果用戶從地址欄或其他瀏覽器組件安裝了PWA，則以下代碼將不起作用 
+deferredPrompt.userChoice.then((choiceResult) => {
+if (choiceResult.outcome === 'accepted') {
+console.log('點擊添加');
+} else {
+console.log('取消添加');
+}
+deferredPrompt = null;
+});
+});
+});
+// 無論以何種方式安裝 PWA 該事件都會觸發
+// 因此這裡可以用來做埋點
+window.addEventListener('appinstalled', (evt) => {
+console.log('應用安裝');
+});
---
+++ sw2.js
@@ -0,0 +1,52 @@
+﻿self.addEventListener('install', function(e) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/index.js',
+        '/sw2.js',
+        '/manifest.json',
+        '/js/matery.js',
+      ]);
+    })
+  );
+});
+self.addEventListener('install', function(e) {
+  console.log('install success')
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  // e.respondWith(
+  //   caches.match(e.request).then(function(response) {
+  //     return response || fetch(e.request);
+  //   })
+  // );
+});
+
+
+
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(e.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(e.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+      });
+    }
+  }));
+});
